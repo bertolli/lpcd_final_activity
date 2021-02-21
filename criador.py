136b9d6bd13d892b2ccd41003f22aaccddc7b52a
Pedro Bertolli <phbertolli@gmail.com>
2021-02-21 10:29:37 -0500

    refactor

--- criador.py
+++ criador.py
@@ -1,79 +1,93 @@
 import psycopg2
+import sys
 
-#Set your local variables here!
-params = dict(
-    database = "postgres",
-    user = "postgres",
-    host = "127.0.0.1",
-    port = "5432",
-    password = "root")
+#This .py connects to the localhost postgresql and creates a new database "grupo_e" that will be used to create tables
+#Set your db configs on main function
 
 def connect_db(params_dic):
     conn = None
     try:
         # connect to the PostgreSQL server
-        print('Connecting to the %s database...' % params_dic.get("database"), end='')
+        print('%s database...' % params_dic.get("database"), end='')
         conn = psycopg2.connect(**params_dic)
         conn.autocommit = True
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+    except (Exception) as error:
+        print("Unable to connect ", error)
         sys.exit(1)
     print("Connection successful!")
     return conn, conn.cursor()
 
-#establishing the connection
-conn, cursor = connect_db(params)
 
-#Create db grupo_e
-cursor.execute('''DROP DATABASE IF EXISTS grupo_e''')
-sql_create = '''CREATE database grupo_e''';
+def execute_sql(conn, cursor, sql_list):
+    try:
+        for sql in sql_list:
+            cursor.execute(sql)
+    except (Exception) as error:
+        print(error)
+    finally:
+        conn.close()
+        cursor.close()
 
-cursor.execute(sql_create)
-print("Database grupo_e created successfully!")
-conn.close()
-cursor.close()
 
-#connecting on created db grupo_e
-params.update(database="grupo_e")
-conn,cursor = connect_db(params)
+def main():
 
-#Drop before create
-cursor.execute("DROP TABLE IF EXISTS registro_covid")
-cursor.execute("DROP TABLE IF EXISTS cidade")
+    # Set your local variables here!
+    params = dict(
+        database="postgres",
+        user="postgres",
+        host="127.0.0.1",
+        port="5432",
+        password="root")
 
-#Creating table cidade
-sql_cidade ='''CREATE TABLE cidade(
-	cod_ibge bigint not null,
-	nome varchar,
-	populacao bigint,
-	estado varchar(2),
-	CONSTRAINT cidade_pk PRIMARY KEY(cod_ibge)
-)'''
-cursor.execute(sql_cidade)
-print("Table cidade created successfully!")
+    sql_cidade = '''CREATE TABLE cidade(
+    	cod_ibge bigint not null,
+    	nome varchar,
+    	populacao bigint,
+    	estado varchar(2),
+    	CONSTRAINT cidade_pk PRIMARY KEY(cod_ibge)
+    )'''
 
-#Creating table registro_covid
-sql_covid ='''create table registro_covid(
-	"data" date,
-	cod_ibge bigint,
-	last_available_confirmed bigint,
-	last_available_deaths bigint,
-	epidemiological_week int,
-	new_confirmed bigint,
-	new_deaths bigint,
-	last_available_date date,
-	last_available_death_rate real,
-	CONSTRAINT registro_covid_pk PRIMARY KEY("data", cod_ibge),
-	CONSTRAINT cidade_fk FOREIGN KEY (cod_ibge) references cidade (cod_ibge)
-)'''
-cursor.execute(sql_covid)
-print("Table registro_covid created successfully!")
+    sql_covid = '''create table registro_covid(
+    	"data" date,
+    	cod_ibge bigint,
+    	last_available_confirmed bigint,
+    	last_available_deaths bigint,
+    	epidemiological_week int,
+    	new_confirmed bigint,
+    	new_deaths bigint,
+    	last_available_date date,
+    	last_available_death_rate real,
+    	CONSTRAINT registro_covid_pk PRIMARY KEY("data", cod_ibge),
+    	CONSTRAINT cidade_fk FOREIGN KEY (cod_ibge) references cidade (cod_ibge)
+    )'''
 
-#Closing the connection
-conn.close()
-cursor.close()
-print("Closing connection")
+    #Connection postgresql
+    conn, cursor = connect_db(params)
 
+    sql_list = list()
+
+    #Create database grupo_e
+    sql_list.append('''DROP DATABASE IF EXISTS grupo_e''')
+    sql_list.append('''CREATE database grupo_e''')
+    execute_sql(conn, cursor, sql_list)
+    print("Database grupo_e created successfully!")
+
+    # Connect database grupo_e
+    params.update(database="grupo_e")
+    conn, cursor = connect_db(params)
+
+    #Create tables
+    sql_list.clear()
+    sql_list.append("DROP TABLE IF EXISTS registro_covid CASCADE")
+    sql_list.append("DROP TABLE IF EXISTS cidade CASCADE")
+    sql_list.append(sql_cidade)
+    sql_list.append(sql_covid)
+    execute_sql(conn, cursor, sql_list)
+    print("Tables created successfully!")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
